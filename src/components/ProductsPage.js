b0bcd1eaded250568050740d1f54064d21c39899
wise-guru <wise.guru123@gmail.com>
2022-08-24 22:16:57 -0400

    initial commit

---
+++ src/components/ProductsPage.js
@@ -0,0 +1,17 @@
+function ProductsPage() {
+  //   {
+  //     products.filter(
+  //       product(product.route.includes(this.props.location)).map(
+  //         (filteredProduct) => <div>{props.name}</div>
+  //       )
+  //     );
+  //   }
+
+  return (
+    <div>
+      <div>Products page</div>
+    </div>
+  );
+}
+
+export default ProductsPage;
