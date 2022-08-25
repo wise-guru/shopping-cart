7bb312ee44a726e4c5d71e726b12a49cc620e8ba
wise-guru <wise.guru123@gmail.com>
2022-08-24 22:14:39 -0400

    Add icons to contact us

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,23 @@
+import Email from "../Assets/icons/email.svg";
+import Instagram from "../Assets/icons/instagram.svg";
+import Twitter from "../Assets/icons/twitter.svg";
+
 function Contact() {
   return (
-    <section>
-      <h1>Contact Us</h1>
-    </section>
+    <main className="contact">
+      <div className="content">
+        <h1>Contact Us</h1>
+        <div className="icons">
+          <img src={Email} alt="letter icon" className="email icon" />
+          <img src={Twitter} alt="twitter icon" className="twitter icon" />
+          <img
+            src={Instagram}
+            alt="instagram icon"
+            className="instagram icon"
+          />
+        </div>
+      </div>
+    </main>
   );
 }
 
