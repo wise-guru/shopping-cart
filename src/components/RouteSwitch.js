32dc28f93fab5901be9f8bc1ca3d112c2a022fdf
wise-guru <wise.guru123@gmail.com>
2022-08-25 22:38:57 -0400

    Add routes for different products

--- src/components/RouteSwitch.js
+++ src/components/RouteSwitch.js
@@ -5,6 +5,7 @@ import Cart from "./Cart";
 import About from "./About";
 import Contact from "./Contact";
 import ProductsPage from "./ProductsPage";
+import SunShaders from "./ProductsPages/SunShaders";
 
 function RouteSwitch() {
   return (
@@ -13,14 +14,24 @@ function RouteSwitch() {
         <Route path="/" element={<Homepage />} />
         <Route path="/about" element={<About />} />
 
-        <Route
+        {/* <Route
           path=":route"
           render={(routeProps) => (
             <ProductsPage route={routeProps.match.params.route} />
           )}
+        /> */}
+        <Route path="/shop/sun-shaders" element={<SunShaders />} />
+        <Route
+          path="/shop/uncomfortable-rain-boots"
+          element={<ProductsPage />}
         />
-        {/* <Route path="/shop/" element={<ProductsPage />} />
-        <Route path="/shop/" element={<ProductsPage />} /> */}
+        <Route path="/shop/shopper-stopper" element={<ProductsPage />} />
+        <Route path="/shop/shoe-umbrella" element={<ProductsPage />} />
+        <Route path="/shop/walking-sleeping-bag" element={<ProductsPage />} />
+        <Route path="/shop/the-baguette-pack" element={<ProductsPage />} />
+        <Route path="/shop/watering-can" element={<ProductsPage />} />
+        <Route path="/shop/airstick" element={<ProductsPage />} />
+        <Route path="/shop/usb-pet-rock" element={<ProductsPage />} />
 
         <Route exact path="/shop/" element={<Shop />} />
         <Route path="/contact" element={<Contact />} />
