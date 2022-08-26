e432b8254d23813fcfca44ce709c2efeaf371761
wise-guru <wise.guru123@gmail.com>
2022-08-25 22:39:30 -0400

    Add data attribute and price

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,10 +2,11 @@ import React from "react";
 
 function Products(props) {
   return (
-    <div className="product">
+    <div className="product" data-id={props.id}>
       <a href={props.route}>
         <img className="productImage" src={props.image} alt={props.name} />
         <div className="productName">{props.name}</div>
+        <div className="productPrice">{props.price}</div>
       </a>
     </div>
   );
