116443c4822ca1f65f350ffb73f89a74bd9aafef
wise-guru <wise.guru123@gmail.com>
2022-08-25 22:41:13 -0400

    format

--- src/components/ProductsPage.js
+++ src/components/ProductsPage.js
@@ -1,17 +1,30 @@
-function ProductsPage() {
-  //   {
-  //     products.filter(
-  //       product(product.route.includes(this.props.location)).map(
-  //         (filteredProduct) => <div>{props.name}</div>
-  //       )
-  //     );
-  //   }
+import Catalog from "./Catalog";
 
+function ProductsPage(props) {
+  console.log(this.props.location);
   return (
+    // <div>
+    //   {Catalog.filter(
+    //     product)product.route.includes(this.props.location)).map(
+    //       (filteredProduct) => <div>{props.name}</div>
+    //     )
+    //   )}
+    // </div>
+
     <div>
-      <div>Products page</div>
+      {Catalog.filter((product) =>
+        product.route.includes(this.props.location)
+      ).map((filteredProduct) => {
+        <div>{filteredProduct.name}</div>;
+      })}
     </div>
   );
 }
+//   return (
+//     <div>
+//       <div>Products page</div>
+//       <div>{props.name}</div>
+//     </div>
+//   );
 
 export default ProductsPage;
