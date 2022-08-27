92098573508e16de0db129825f860dbd3111e142
wise-guru <wise.guru123@gmail.com>
2022-08-26 22:26:12 -0400

    Add data-id

--- src/components/Products.js
+++ src/components/Products.js
@@ -4,9 +4,18 @@ function Products(props) {
   return (
     <div className="product" data-id={props.id}>
       <a href={props.route}>
-        <img className="productImage" src={props.image} alt={props.name} />
-        <div className="productName">{props.name}</div>
-        <div className="productPrice">{props.price}</div>
+        <img
+          className="productImage"
+          src={props.image}
+          alt={props.name}
+          data-id={props.id}
+        />
+        <div className="productName" data-id={props.id}>
+          {props.name}
+        </div>
+        <div className="productPrice" data-id={props.id}>
+          {props.price}
+        </div>
       </a>
     </div>
   );
