969af5a899f2d940f5f841f7519c99239e2264bb
wise-guru <wise.guru123@gmail.com>
2022-08-26 22:27:39 -0400

    Add individual route pages for products

--- src/components/RouteSwitch.js
+++ src/components/RouteSwitch.js
@@ -1,11 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./Homepage";
 import Shop from "./Shop";
-import Cart from "./Cart";
 import About from "./About";
 import Contact from "./Contact";
-import ProductsPage from "./ProductsPage";
 import SunShaders from "./ProductsPages/SunShaders";
+import WateringCan from "./ProductsPages/WateringCan";
+import ShopperStopper from "./ProductsPages/ShopperStopper";
+import ShoeUmbrellas from "./ProductsPages/ShoeUmbrellas";
+import SleepingBag from "./ProductsPages/SleepingBag";
+import Baguette from "./ProductsPages/Baguette";
+import Airstick from "./ProductsPages/Airstick";
+import Rock from "./ProductsPages/Rock";
+import Boots from "./ProductsPages/Boots";
+import Checkout from "./Checkout";
 
 function RouteSwitch() {
   return (
@@ -21,21 +28,18 @@ function RouteSwitch() {
           )}
         /> */}
         <Route path="/shop/sun-shaders" element={<SunShaders />} />
-        <Route
-          path="/shop/uncomfortable-rain-boots"
-          element={<ProductsPage />}
-        />
-        <Route path="/shop/shopper-stopper" element={<ProductsPage />} />
-        <Route path="/shop/shoe-umbrella" element={<ProductsPage />} />
-        <Route path="/shop/walking-sleeping-bag" element={<ProductsPage />} />
-        <Route path="/shop/the-baguette-pack" element={<ProductsPage />} />
-        <Route path="/shop/watering-can" element={<ProductsPage />} />
-        <Route path="/shop/airstick" element={<ProductsPage />} />
-        <Route path="/shop/usb-pet-rock" element={<ProductsPage />} />
+        <Route path="/shop/uncomfortable-rain-boots" element={<Boots />} />
+        <Route path="/shop/shopper-stopper" element={<ShopperStopper />} />
+        <Route path="/shop/shoe-umbrella" element={<ShoeUmbrellas />} />
+        <Route path="/shop/walking-sleeping-bag" element={<SleepingBag />} />
+        <Route path="/shop/the-baguette-pack" element={<Baguette />} />
+        <Route path="/shop/watering-can" element={<WateringCan />} />
+        <Route path="/shop/airstick" element={<Airstick />} />
+        <Route path="/shop/usb-pet-rock" element={<Rock />} />
 
         <Route exact path="/shop/" element={<Shop />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
       </Routes>
     </BrowserRouter>
   );
