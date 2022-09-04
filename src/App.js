a03366577456da451b47e1c7f2c3352f255081c6
wise-guru <wise.guru123@gmail.com>
2022-09-04 18:35:19 -0400

    Add cart array

--- src/App.js
+++ src/App.js
@@ -1,13 +1,58 @@
+import { useState } from "react";
+import { BrowserRouter, Link } from "react-router-dom";
+// import x from "uniqid";
 import "./App.css";
+import Catalog from "./components/Catalog";
 import Navigation from "./components/Navigation";
 import RouteSwitch from "./components/RouteSwitch";
 
 function App() {
+  const [cartItems, setCartItems] = useState([]);
+  const products = Catalog;
+  const totalItems = cartItems.reduce((a, c) => a + c.qty, 0);
+
+  const addProduct = (product) => {
+    const exist = cartItems.find((item) => item.id === product.id);
+
+    if (exist) {
+      const newCartItems = cartItems.map((item) =>
+        item.id === product.id ? { ...exist, qty: exist.qty + 1 } : item
+      );
+      setCartItems(newCartItems);
+    } else {
+      const newCartItems = [...cartItems, { ...product, qty: +1 }];
+      setCartItems(newCartItems);
+    }
+  };
+  const removeProduct = (product) => {
+    const exist = cartItems.find((item) => item.id === product.id);
+    if (exist.qty === 1) {
+      const newCartItems = cartItems.filter((item) => item.id !== product.id);
+      setCartItems(newCartItems);
+    } else {
+      const newCartItems = cartItems.map((item) =>
+        item.id === product.id ? { ...exist, qty: exist.qty - 1 } : item
+      );
+      setCartItems(newCartItems);
+    }
+  };
+
+  // const resetCartItems = this.state.push
+
   return (
-    <div className="container">
-      <Navigation />
-      <RouteSwitch />
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <Navigation cartQuantity={totalItems} />
+        <RouteSwitch
+          addProduct={addProduct}
+          removeProduct={removeProduct}
+          setCartItems={setCartItems}
+          products={products}
+          cartItems={cartItems}
+          totalItems={totalItems}
+        />
+      </div>
+    </BrowserRouter>
   );
 }
 
